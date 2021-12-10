3b4e7cba7ebffafb2a1b29eb15546c9f312765a8
Youssef EL Yamani <yelyamani@pca.ma>
2021-12-10 19:41:52 +0100

    QuickFindUf

---
+++ src/union_find/QuickUnionQU.java
@@ -0,0 +1,28 @@
+package union_find;
+
+public class QuickUnionQU {
+    int[] id;
+
+    public QuickUnionQU(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++)
+            id[i] = i;
+    }
+
+    private int root(int i) {
+        while (i != id[i]) i = id[i];
+        return i;
+    }
+
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+    }
+
+
+}
