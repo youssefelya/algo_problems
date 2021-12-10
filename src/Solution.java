f7a4747cfd88bf252e04cdb8bd4517d777663f5e
Youssef EL Yamani <yelyamani@pca.ma>
2021-12-10 19:29:00 +0100

    QuickFindUf

--- src/Solution.java
+++ src/Solution.java
@@ -8,19 +8,6 @@ public class Solution {
     }
 
 
-    public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return helper(nums, 0, dp);
-    }
-
-    int helper(int[] nums, int i, int[] dp) {
-        if (i >= nums.length) return 0;
-        if (dp[i] != -1)
-            return dp[i];
-        dp[i] = Math.max(nums[i] + helper(nums, i + 2, dp), helper(nums, i + 1, dp));
-        return dp[i];
-    }
 
 
 }
