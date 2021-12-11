9056db30c72d26b854872147e01bc511b9d898d9
Youssef EL Yamani <yelyamani@pca.ma>
2021-12-11 12:45:06 +0100

    QuickFindUf unionQ

--- src/union_find/QuickUnionQU.java
+++ src/union_find/QuickUnionQU.java
@@ -2,11 +2,15 @@ package union_find;
 
 public class QuickUnionQU {
     int[] id;
+    int[] size;
 
     public QuickUnionQU(int N) {
         id = new int[N];
-        for (int i = 0; i < N; i++)
+        size = new int[N];
+        for (int i = 0; i < N; i++) {
             id[i] = i;
+            size[i] = 1;
+        }
     }
 
     private int root(int i) {
@@ -22,6 +26,21 @@ public class QuickUnionQU {
         int i = root(p);
         int j = root(q);
         id[i] = j;
+    }
+
+    // to make the root of every tree balance !!
+    // to avoid that
+    public void unionQ(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        if (i == j) return;
+        if (size[i] < size[j]) {
+            id[i] = j;
+            size[j] += size[i];
+        } else {
+            id[j] = i;
+            size[i] += size[j];
+        }
     }
 
 
