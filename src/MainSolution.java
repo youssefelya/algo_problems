8dda530f976cdb90810ecb449c65373c4203d0e3
Youssef EL Yamani <yelyamani@pca.ma>
2021-12-13 08:23:28 +0100

    he power of the string

--- src/MainSolution.java
+++ src/MainSolution.java
@@ -9,21 +9,17 @@ public class MainSolution {
         int[] arr = {1, 2, 5};
 
     }
-
-    public boolean canPartition(int[] nums) {
-        int sum = Arrays.stream(nums).sum();
-        return (sum % 2 == 0) && subsetSum(nums, 0, new HashMap<>(), sum / 2);
-    }
-
-    private boolean subsetSum(int[] nums, int i, Map<String, Boolean> memo, int sum) {
-        if (sum == 0)
-            return true;
-        if (i >= nums.length || sum < 0)
-            return false;
-        String key = i + "," + sum;
-        if (memo.containsKey(key))
-            return memo.get(key);
-        memo.put(key, subsetSum(nums, i + 1, memo, sum - nums[i]) || subsetSum(nums, i + 1, memo, sum));
-        return memo.get(key);
+    public int maxPower(String s) {
+        int n = s.length();
+        int max = -1, i=0;
+        while(i<n){
+            char c = s.charAt(i);
+            int count=0;
+            int j=i;
+            while(j<n && c == s.charAt(j)){count++; j++;}
+            max = Math.max(count,max);
+            i=j;
+        }
+        return max;
     }
 }
