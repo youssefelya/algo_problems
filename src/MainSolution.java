e076c88311c459d01224e336893f2ad69c112fbd
Youssef EL Yamani <yelyamani@pca.ma>
2021-12-14 08:09:08 +0100

    rangeSumBST

--- src/MainSolution.java
+++ src/MainSolution.java
@@ -1,3 +1,5 @@
+import data_struct.TreeNode;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -5,21 +7,20 @@ import java.util.Map;
 public class MainSolution {
 
     public static void main(String[] args) {
-//[1,2,5]
-        int[] arr = {1, 2, 5};
+
 
     }
-    public int maxPower(String s) {
-        int n = s.length();
-        int max = -1, i=0;
-        while(i<n){
-            char c = s.charAt(i);
-            int count=0;
-            int j=i;
-            while(j<n && c == s.charAt(j)){count++; j++;}
-            max = Math.max(count,max);
-            i=j;
-        }
-        return max;
+
+    /*
+    Given the root node of a binary search tree and two integers low and high,
+     return the sum of values of all nodes with a value in the inclusive range [low, high].
+     */
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) return 0;
+        int v = 0;
+        if (root.val >= low && root.val <= high) v = root.val;
+        return v + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
     }
+
+
 }
