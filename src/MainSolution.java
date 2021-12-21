c175fd14b285099ab8630f6282b095ebf14a2375
Youssef EL Yamani <yelyamani@pca.ma>
2021-12-21 22:46:58 +0100

    SnapshotArray

--- src/MainSolution.java
+++ src/MainSolution.java
@@ -1,4 +1,5 @@
 import data_struct.ListNode;
+import edu.princeton.cs.algs4.In;
 
 import java.util.*;
 
@@ -9,44 +10,36 @@ public class MainSolution {
 
     }
 
-    public int firstBadVersion(int n) {
-        int left =0;
-        int right = n;
-        //int mid = left + (right-left)/2;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (isBadVersion(mid)) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
+    //https://leetcode.com/problems/snapshot-array/submissions/
+    class SnapshotArray {
+        Map<Integer, Map<Integer, Integer>> map;
+        int snapId;
+        public SnapshotArray(int length) {
+            map = new HashMap<>();
+            snapId = 0;
         }
 
-        int low = 0;
-        int high = n - 1;
-        int mid = (low + high) >>> 1;
-        while (low <= high) {
-            mid = (low + high) >>> 1;
-            boolean version = isBadVersion(mid);
-            if (version) {
-                if (mid > 0 && !isBadVersion(mid - 1))
-                    return mid;
-                else if (mid == 0) return mid;
-                low = mid + 1;
-            } else {
-                if (mid + 1 < n && isBadVersion(mid + 1))
-                    return mid;
-                else if (mid == n - 1) return mid;
-                high = mid - 1;
-            }
+        public void set(int index, int val) {
+            map.computeIfAbsent(index, x -> new HashMap<>());
+            map.get(index).put(snapId, val);
+
         }
-        return -1;
+
+        public int snap() {
+            return snapId++;
+        }
+
+        public int get(int index, int snap_id) {
+            if (map.containsKey(index)) {
+                Map<Integer, Integer> H1 = map.get(index);
+                for (int gg = snap_id; gg >= 0; gg--)
+                    if (H1.containsKey(gg)) {
+                        return H1.get(gg);
+                    }
+                return 0;
+            }
+            return 0;
+        }
     }
-
-
-    static boolean isBadVersion(int b) {
-        return false;
-    }
-
 
 }
