cd937bc9aa13fc91ee75f6b6afcd67fcdb33a145
Youssef EL Yamani <yelyamani@pca.ma>
2021-12-30 08:48:52 +0100

    addBinary

--- src/MainSolution.java
+++ src/MainSolution.java
@@ -8,85 +8,33 @@ public class MainSolution {
 
 
     public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(new FileInputStream("")));
+        String a = "10";
+        String b = "11";
+        System.out.println(addBinary(a, b));
     }
 
-    /*
-            [5,4,3,2,1,9]
-     */
+    public static String addBinary(String a, String b) {
+        return addBinaryHelper(a, a.length() - 1, b, b.length() - 1, 0);
+    }
 
-    public long getDescentPeriods(int[] prices) {
-        long answer = 0;
-        int i = 0, n = prices.length;
-        while (i < n) {
-            int prev = prices[i];
-            int index = i + 1;
-            while (index < n && prev == prices[index] + 1) {
-                prev = prices[index];
-                index++;
+    private static String addBinaryHelper(String a, int i, String b, int j, int res) {
+        if (i < 0 && j < 0) {
+            StringBuilder str = new StringBuilder();
+            while (res > 0) {
+                str.append(res % 2);
+                res = res / 2;
             }
-            if (index >= n) {
-                index--;
-
-            }
-            i = index;
+            return str.reverse().toString();
+        } else if (i < 0) {
+            int sum = Character.getNumericValue(a.charAt(j)) + res;
+            return addBinaryHelper(a, i - 1, b, j - 1, sum/2) + "" + (sum % 2);
+        } else if (j < 0) {
+            int sum = Character.getNumericValue(b.charAt(i)) + res;
+            return addBinaryHelper(a, i - 1, b, j - 1, sum/2) + "" + (sum % 2);
+        } else {
+            int sum = Character.getNumericValue(a.charAt(i)) + res + Character.getNumericValue(b.charAt(j));
+            return addBinaryHelper(a, i - 1, b, j - 1, sum/2) + (sum % 2);
         }
-        return answer;
-    }
-
-
-    public int kIncreasing(int[] arr, int k) {
-        int n = arr.length;
-        int[][] dp = new int[n][n];
-        for (int i = 0; i < n; i++)
-            Arrays.fill(dp[i], -1);
-        return kIncreasingHelper(arr, 1, 0, k, dp);
-    }
-
-    private int kIncreasingHelper(int[] arr, int lastSeen, int first, int end, int[][] dp) {
-        if (end >= arr.length) return 0;
-        if (end == arr.length - 1 && arr[first] <= arr[end]) return 0;
-        if (dp[first][end] != -1) return dp[first][end];
-        int val = arr[first];
-        arr[first] = 1;
-        int sol1 = kIncreasingHelper(arr, 1, first + 1, end + 1, dp) + (val != 1 ? 1 : 0);
-        arr[first] = val;
-        int sol2 = kIncreasingHelper(arr, Math.max(arr[first], lastSeen), first + 1, end + 1, dp) + (arr[first] <= arr[end] ? 0 : 1);
-        dp[first][end] = Math.max(sol1, sol2);
-        return dp[first][end];
-    }
-
-
-    public String firstPalindrome(String[] words) {
-        int n = words.length;
-        for (String word : words)
-            if (isPalindrome(word, word.length() - 1)) return word;
-        return "";
-    }
-
-    private boolean isPalindrome(String word, int n) {
-        int i = 0;
-        while (i < n) {
-            if (word.charAt(i) != word.charAt(n)) return false;
-            n--;
-            i++;
-        }
-        return true;
-    }
-
-    public String addSpaces(String s, int[] spaces) {
-        int n = s.length();
-        int j = 0;
-        StringBuilder str = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            if (j < spaces.length && i == spaces[j]) {
-                str.append(" ");
-                j++;
-            }
-            str.append(s.charAt(i));
-
-        }
-        return str.toString();
     }
 
 
