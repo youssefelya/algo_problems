13c18e418477c5853d02dd29f2eda890bd3f9aeb
Youssef EL Yamani <yelyamani@pca.ma>
2021-12-30 16:31:39 +0100

    serialize serialize

--- src/MainSolution.java
+++ src/MainSolution.java
@@ -1,40 +1,57 @@
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import data_struct.TreeNode;
+
 import java.util.*;
 
 public class MainSolution {
 
 
     public static void main(String[] args) throws Exception {
-        String a = "10";
-        String b = "11";
-        System.out.println(addBinary(a, b));
+        String s = "1,2,3,4,55,3,null,33,4";
+        System.out.println(s);
+
     }
 
-    public static String addBinary(String a, String b) {
-        return addBinaryHelper(a, a.length() - 1, b, b.length() - 1, 0);
-    }
-
-    private static String addBinaryHelper(String a, int i, String b, int j, int res) {
-        if (i < 0 && j < 0) {
-            StringBuilder str = new StringBuilder();
-            while (res > 0) {
-                str.append(res % 2);
-                res = res / 2;
+    // Encodes a tree to a single string.
+    public static String serialize(TreeNode root) {
+        if (root == null)
+            return null;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        List<String> list = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            TreeNode t = stack.pop();
+            if (t == null) {
+                list.add("#");
+            } else {
+                list.add("" + t.val);
+                stack.push(t.right);
+                stack.push(t.left);
             }
-            return str.reverse().toString();
-        } else if (i < 0) {
-            int sum = Character.getNumericValue(a.charAt(j)) + res;
-            return addBinaryHelper(a, i - 1, b, j - 1, sum/2) + "" + (sum % 2);
-        } else if (j < 0) {
-            int sum = Character.getNumericValue(b.charAt(i)) + res;
-            return addBinaryHelper(a, i - 1, b, j - 1, sum/2) + "" + (sum % 2);
-        } else {
-            int sum = Character.getNumericValue(a.charAt(i)) + res + Character.getNumericValue(b.charAt(j));
-            return addBinaryHelper(a, i - 1, b, j - 1, sum/2) + (sum % 2);
         }
+        return String.join(",", list);
+    }
+
+    static int t;
+    public static TreeNode deserialize(String data) {
+        if (data == null)
+            return null;
+        t = 0;
+        String[] arr = data.split(",");
+        return deserializeHelper(arr);
+    }
+
+    public static TreeNode deserializeHelper(String[] arr) {
+        if (arr[t].equals("#"))
+            return null;
+        // create node with this item and recur for children
+        TreeNode root
+                = new TreeNode(Integer.parseInt(arr[t]));
+        t++;
+        root.left = deserializeHelper(arr);
+        t++;
+        root.right = deserializeHelper(arr);
+        return root;
     }
 
 
