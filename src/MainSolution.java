e34fb1a8704bda1a9aefb8c107e7a9c0e1587a17
Youssef EL Yamani <yelyamani@pca.ma>
2022-01-01 22:05:44 +0100

    second problem lengthOfLongestSubstring

--- src/MainSolution.java
+++ src/MainSolution.java
@@ -7,7 +7,30 @@ import java.util.stream.Collectors;
 public class MainSolution {
 
     public static void main(String[] args) {
+        String s = "pwwkew";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
 
+
+    //https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0;
+        int i = 0, toDelete = 0;
+        while (i < n) {
+            char c = s.charAt(i);
+            if (set.contains(c)) {
+                while (set.contains(c)) {
+                    set.remove(s.charAt(toDelete));
+                    toDelete++;
+                }
+            }
+            set.add(c);
+            ans = Math.max(ans, set.size());
+            i++;
+        }
+        return ans;
     }
 
     //https://leetcode.com/problems/two-sum/
@@ -21,5 +44,6 @@ public class MainSolution {
         }
         return new int[]{};
     }
+
 
 }
