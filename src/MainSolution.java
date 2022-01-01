2e3f04983ef13ff9111434730d4099fbb4806819
Youssef EL Yamani <yelyamani@pca.ma>
2022-01-01 22:56:55 +0100

    longestPalindrome

--- src/MainSolution.java
+++ src/MainSolution.java
@@ -7,8 +7,37 @@ import java.util.stream.Collectors;
 public class MainSolution {
 
     public static void main(String[] args) {
-        String s = "pwwkew";
-        System.out.println(lengthOfLongestSubstring(s));
+
+    }
+
+    //https://leetcode.com/problems/longest-palindromic-substring/
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        int start = 0, end = 0, length = 0;
+        for (int i = 0; i < n; i++) {
+            int[] sol1 = palindrome(s, i, i + 1);
+            int[] sol2 = palindrome(s, i, i);
+            if (sol1[1] - sol1[0] > length) {
+                length = sol1[1] - sol1[0];
+                start = sol1[0];
+                end = sol1[1];
+            }
+            if (sol2[1] - sol2[0] > length) {
+                length = sol2[1] - sol2[0];
+                start = sol2[0];
+                end = sol2[1];
+            }
+
+        }
+        return s.substring(start, end);
+    }
+
+    private int[] palindrome(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return new int[]{left + 1, right};
     }
 
 
