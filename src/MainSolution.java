abfd73bca95c0c1bf8b01a405627f3642b069486
Youssef EL Yamani <yelyamani@pca.ma>
2022-01-01 18:48:42 +0100

    Threading

--- src/MainSolution.java
+++ src/MainSolution.java
@@ -2,24 +2,71 @@
 import data_struct.TreeNode;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class MainSolution {
 
-    public static void main(String[] args) {
+    static List<Integer> list = new ArrayList<>();
+    static int index = 0;
 
+    static class Threading extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 3; i++) {
+                list.add(i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
-    public int maxAncestorDiff(TreeNode root) {
-        if (root == null) return 0;
-        int v1 = bfs(root.left, root.val, root.val);
-        int v2 = bfs(root.right, root.val, root.val);
-        return Math.max(v1, v2);
+    public static void main(String[] args) throws InterruptedException {
+        Threading thread = new Threading();
+        Threading thread2 = new Threading();
+        thread.start();
+        thread2.start();
+        Thread.sleep(5000);
+        System.out.println(index);
+        System.out.println(list);
     }
 
-    private int bfs(TreeNode root, int min, int max) {
-        if (root == null) return max - min;
-        return Math.max(bfs(root.left, Math.min(min, root.val), Math.max(max, root.val)),
-                bfs(root.right, Math.min(min, root.val), Math.max(max, root.val)));
+
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (mid > 0 && nums[mid] != nums[mid - 1] && mid + 1 < n && nums[mid + 1] != nums[mid]) return mid;
+            if (mid > 0 && nums[mid] == nums[mid - 1]) {
+                if ((mid - 1 - low) % 2 == 1)
+                    high = mid - 1;
+                else low = mid + 1;
+            } else if (mid + 1 < n && nums[mid] == nums[mid + 1]) {
+                if ((high - mid + 1) % 2 == 1)
+                    low = mid - 1;
+                else high = mid + 1;
+            } else return mid;
+        }
+        return 1;
+    }
+
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        bfs(root, 0);
+        return map.keySet().stream().map(x -> map.get(x)).collect(Collectors.toList());
+    }
+
+    Map<Integer, List<Integer>> map = new HashMap<>();
+
+    private void bfs(TreeNode root, int level) {
+        if (root == null) return;
+        map.computeIfAbsent(level, x -> new ArrayList<>());
+        map.get(level).add(root.val);
+        bfs(root.left, level + 1);
+        bfs(root.right, level + 1);
     }
 
 
