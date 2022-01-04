7e42ba4dc13e0a62589c385854ef14925fc68fd2
Youssef EL Yamani <yelyamani@pca.ma>
2022-01-04 22:23:05 +0100

    searchRange

--- src/Solution.java
+++ src/Solution.java
@@ -6,25 +6,33 @@ import java.util.*;
 
 public class Solution {
 
-    /*
-    Three pairs have a total duration divisible by 60:
-(time[0] = 30, time[2] = 150): total duration 180
-(time[1] = 20, time[3] = 100): total duration 120
-(time[1] = 20, time[4] = 40): total duration 60
-     */
+    static List<String> list = new ArrayList<>();
+    static Vector<String> vector = new Vector<>();
 
-    public static void main(String[] args) {
-        int[] time = {30, 20, 150, 100, 40};
-        for (int i = 0; i < time.length; i++)
-            time[i] = time[i] % 60;
-        System.out.println(Arrays.toString(time));
+    static class Threading extends Thread {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 3; i++) {
+                list.add(Thread.currentThread().getName() + "_" );
+                vector.add(Thread.currentThread().getName() + "_" );
+                System.out.print(i + " ");
+            }
+        }
     }
 
-    public int numPairsDivisibleBy60(int[] time) {
-        int ans = 0, cnt[] = new int[60];
-        for (int i = 0; i < time.length; i++) cnt[time[i] % 60] += 1;
-        for (int i = 1; i < 30; i++) ans += cnt[i] * cnt[60 - i];
-        return ans + cnt[0] * (cnt[0] - 1) / 2 + cnt[30] * (cnt[30] - 1) / 2;
+
+    public static void main(String[] args) throws InterruptedException {
+        Threading tr = new Threading();
+        Threading tr2 = new Threading();
+        tr.start();
+        tr2.start();
+
+        Thread.sleep(3000);
+        System.out.println();
+        System.out.println("list : "+list);
+        System.out.println("vector : " + vector);
     }
+
 
 }
