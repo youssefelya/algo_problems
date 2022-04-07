50a992c0d1d4bdf6f11d3e8345bc4aa616dd8a22
Youssef EL Yamani <yelyamani@pca.ma>
2022-04-07 09:14:38 +0000

    rob house

--- src/Solution.java
+++ src/Solution.java
@@ -1,51 +1,38 @@
+
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Solution {
 
 
     public static void main(String[] args) {
-
-        int x = 0;
-        String num1 = "12";
-        String num2 = "20";
-        System.out.println(multiply(num1, num2));
+        int[] array = {1,2,3,1};
+        System.out.println(rob(array));
     }
 
-
-    public static String multiply(String num1, String num2) {
-        StringBuilder str = new StringBuilder();
-        String answer = "0";
-        if ("0".equals(num1) || "0".equals(num2)) return "0";
-        if (num1.length() < 1 || num2.length() < 1) return num1 + num2;
-        if (num1.length() == 1) return multiplyHelper(Character.getNumericValue(num1.charAt(0)), num2);
-        if (num2.length() == 1) return multiplyHelper(Character.getNumericValue(num2.charAt(0)), num1);
-        for (int i = num1.length() - 1; i >= 0; i--) {
-            String multiplyOneDigit = multiplyHelper(Character.getNumericValue(num1.charAt(i)), num2);
-            answer = sum(answer, answer.length() - 1, multiplyOneDigit + str, (multiplyOneDigit + str).length() - 1, 0);
-            str.append("0");
+    public static int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (i == 0) dp[i] = nums[i];
+            else {
+                int v = i - 2 >= 0 ? dp[i - 2] : 0;
+                dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+            }
         }
-        return answer;
+        return dp[n-1];
     }
 
-    private static String multiplyHelper(int val, String s) {
-        int res = 0, n = s.length();
-        StringBuilder str = new StringBuilder();
-        for (int i = n - 1; i >= 0; i--) {
-            int prod = (Character.getNumericValue(s.charAt(i)) * val + res);
-            res = prod / 10;
-            str.append(prod % 10);
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int a : stones) maxHeap.add(a);
+        while (maxHeap.size() > 1) {
+            int stone1 = maxHeap.poll();
+            int stone2 = maxHeap.poll();
+            int diff = Math.abs(stone1 - stone2);
+            if (diff != 0) maxHeap.add(diff);
         }
-        if (res > 0) str.append(res);
-        return str.reverse().toString();
-    }
-
-    public static String sum(String num1, int i, String num2, int j, int res) {
-        if (i < 0 && j < 0)
-            return res > 0 ? String.valueOf(res) : "";
-        int v1 = i < 0 ? 0 : Character.getNumericValue(num1.charAt(i));
-        int v2 = j < 0 ? 0 : Character.getNumericValue(num2.charAt(j));
-        int sum = v1 + v2 + res;
-        return sum(num1, i - 1, num2, j - 1, sum / 10) + String.valueOf(sum % 10);
+        return maxHeap.size() > 0 ? maxHeap.peek() : 0;
     }
 
 }
