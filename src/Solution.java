f087b8fc95f4ff4f8c1a9ea6b7fcef62f5fbefd8
Youssef EL Yamani <yelyamani@pca.ma>
2022-04-12 19:53:55 +0000

    gameOfLife

--- src/Solution.java
+++ src/Solution.java
@@ -5,22 +5,45 @@ public class Solution extends Singleton {
 
 
     public static void main(String[] args) {
-        int[] books = {1, 2, 3, 4};
-        int[] books2 = {0, 1, 1, 2, 3, 4};
-        System.out.println(8 + (8 * 5 * 0.75));
+
     }
 
-    public int maxSubArray(int[] nums) {
-        int max = nums[0];
-        int maxGlobal = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            max = Math.max(nums[i], max + nums[i]);
-            if (max > maxGlobal) {
-                maxGlobal = max;
+
+    public void gameOfLife(int[][] board) {
+        Map<int[], Integer> map = new HashMap<>();
+        int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}, {-1, -1}, {1, 1}, {1, -1}, {-1, 1}};
+        int rows = board.length;
+        int cols = board[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int liveNeighbors = 0;
+                for (int[] x : dir) {
+                    int rr = x[0] + i;
+                    int cc = x[1] + j;
+                    if (inRange(rr, cc, rows, cols) && board[rr][cc] == 1) {
+                        liveNeighbors++;
+                    }
+                }
+                map.put(new int[]{i, j}, liveNeighbors);
             }
         }
-        return maxGlobal;
+        for (int[] each : map.keySet()) {
+            int curI = each[0];
+            int curJ = each[0];
+            int liveNeighbors = map.get(each);
+            if (board[curI][curJ] == 1) {
+                if (liveNeighbors < 2 || liveNeighbors > 3) board[curI][curJ] = 0;
+            } else {
+                if (liveNeighbors == 3) {
+                    board[curI][curJ] = 1;
+                }
+            }
+        }
     }
+    boolean inRange(int i, int j, int rows, int cols) {
+        return i >= 0 && j >= 0 && i < rows && j < cols;
+    }
+
 
 }
 
