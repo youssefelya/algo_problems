7438156bdaaac1d727e87aa4abab3b21a0ccc3d3
Youssef EL Yamani <yelyamani@pca.ma>
2022-04-13 09:23:25 +0000

    consecutiveNumbersSum

--- src/Solution.java
+++ src/Solution.java
@@ -5,45 +5,28 @@ public class Solution extends Singleton {
 
 
     public static void main(String[] args) {
-
+        int val1 = 5;
+        int val2 = 2;
+        double division = (double) val1 / val2;
+        System.out.println(division);
     }
 
+    static boolean isInteger(double value) {
+        return value - (int) value == 0;
+    }
 
-    public void gameOfLife(int[][] board) {
-        Map<int[], Integer> map = new HashMap<>();
-        int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}, {-1, -1}, {1, 1}, {1, -1}, {-1, 1}};
-        int rows = board.length;
-        int cols = board[0].length;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                int liveNeighbors = 0;
-                for (int[] x : dir) {
-                    int rr = x[0] + i;
-                    int cc = x[1] + j;
-                    if (inRange(rr, cc, rows, cols) && board[rr][cc] == 1) {
-                        liveNeighbors++;
-                    }
-                }
-                map.put(new int[]{i, j}, liveNeighbors);
-            }
+    public static int consecutiveNumbersSum(int n) {
+        int answer = 0;
+        for (int k = 1; k < n; k++) {
+            double division = (double) (n - k * (k - 1) / 2) / k;
+            if (isValidNumber(division)) answer++;
         }
-        for (int[] each : map.keySet()) {
-            int curI = each[0];
-            int curJ = each[0];
-            int liveNeighbors = map.get(each);
-            if (board[curI][curJ] == 1) {
-                if (liveNeighbors < 2 || liveNeighbors > 3) board[curI][curJ] = 0;
-            } else {
-                if (liveNeighbors == 3) {
-                    board[curI][curJ] = 1;
-                }
-            }
-        }
-    }
-    boolean inRange(int i, int j, int rows, int cols) {
-        return i >= 0 && j >= 0 && i < rows && j < cols;
+        return answer;
     }
 
+    private static boolean isValidNumber(double division) {
+        return division > 0 && division - (int) division == 0;
+    }
 
 }
 
