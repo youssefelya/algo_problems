43d1166d3e507782f20dc3d1146d37c8ff2e2a4d
Youssef EL Yamani <yelyamani@pca.ma>
2022-04-13 23:28:23 +0000

    traverseNode

--- src/Solution.java
+++ src/Solution.java
@@ -5,35 +5,56 @@ public class Solution extends Singleton {
 
 
     public static void main(String[] args) {
-        int n = 6;
-        for(int[] arr : generateMatrix(n))
-            System.out.println(Arrays.toString(arr));
+        char[][] grid = {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+        System.out.println(numIslands(grid));
+
     }
 
-    public static int[][] generateMatrix(int n) {
-        int[][] mat = new int[n][n];
-        int val = 1;
-        int toLeft = 0, topRight = n - 1, bottomLeft = 0, bottomRight = n - 1;
-        int i, j = 0;
-        while (toLeft <= topRight) {
-            i = toLeft;
-            toLeft++;
-            while (j <= topRight) mat[i][j++] = val++;
-            topRight--;
-            j--;
-            i++;
-            while (i <= bottomRight) mat[i++][j] = val++;
-            bottomRight--;
-            i--;
-            j--;
-            while (j >= bottomLeft) mat[i][j--] = val++;
-            bottomLeft++;
-            j++;
-            i--;
-            while (i >= toLeft) mat[i--][j] = val++;
-            j++;
+    public static int numIslands(char[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int answer = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '0' || visited[i][j]) continue;
+                answer++;
+                traverseNode(grid, i, j, visited);
+            }
         }
-        return mat;
+        return answer;
+    }
+
+    private static void traverseNode(char[][] grid, int i, int j, boolean[][] visited) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{i, j});
+        while (queue.size() > 0) {
+            int[] nodeToExplore = queue.poll();
+            int currentI = nodeToExplore[0];
+            int currentJ = nodeToExplore[1];
+            if (visited[currentI][currentJ] || grid[currentI][currentJ] == '0') continue;
+            visited[currentI][currentJ] = true;
+            queue.addAll(getNodeNeighbors(grid, currentI, currentJ, visited));
+        }
+    }
+
+    private static Queue<int[]> getNodeNeighbors(char[][] grid, int currentI, int currentJ, boolean[][] visited) {
+        int[][] mat = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        Queue<int[]> queue = new ArrayDeque<>();
+        for (int[] arr : mat) {
+            int nextI = currentI + arr[0];
+            int nextJ = currentJ + arr[1];
+
+            if (nextI < grid.length && nextI >= 0 && !visited[nextI][currentJ])
+                queue.add(new int[]{nextI, currentJ});
+            if (nextJ < grid[0].length && nextJ >= 0 && !visited[currentI][nextJ])
+                queue.add(new int[]{currentI, nextJ});
+        }
+        return queue;
     }
 
 }
