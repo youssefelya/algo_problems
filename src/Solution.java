3adf3a61cd85c6576f754c283e976f22acb8354e
Youssef EL Yamani <yelyamani@pca.ma>
2022-04-18 20:32:12 +0000

    sumOfThree

--- src/Solution.java
+++ src/Solution.java
@@ -2,6 +2,7 @@
 import data_struct.ListNode;
 import data_struct.TreeNode;
 
+import javax.swing.*;
 import java.util.*;
 
 public class Solution extends Singleton {
@@ -12,37 +13,19 @@ public class Solution extends Singleton {
                 {1, 2, 3},
                 {2, 3, 1},
                 {1, 6, 3}};
-        NumMatrix matrix = new NumMatrix(mat);
+
 
     }
 
-    static class NumMatrix {
-        int[][] matrix;
+    public long[] sumOfThree(long num) {
+        double result = ((double) num - 3) / 3;
+        long ans = (long) result;
+        if (isLong(result)) return new long[]{ans, ans + 1, ans + 2};
+        return new long[]{};
+    }
 
-        public NumMatrix(int[][] matrix) {
-
-            int row = matrix.length, col = matrix[0].length;
-            this.matrix = new int[row][col];
-            for (int i = 0; i < row; i++) {
-                int sum = matrix[i][0];
-                this.matrix[i][0] = matrix[i][0];
-                for (int j = 1; j < col; j++) {
-                    sum = (matrix[i][j] + sum);
-                    this.matrix[i][j] = sum;
-                }
-            }
-
-        }
-
-        public int sumRegion(int row1, int col1, int row2, int col2) {
-            int answer = 0;
-            for (int i = row1; i <= row2; i++) {
-                if (col1 - 1 >= 0)
-                    answer += (matrix[i][col2] - matrix[i][col1 - 1]);
-                else answer += matrix[i][col2];
-            }
-            return answer;
-        }
+    private boolean isLong(double result) {
+        return (long) result == result;
     }
 
 
