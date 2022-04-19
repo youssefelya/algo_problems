9edbb3784de95c78b656a024cb84e2963ddeea9e
Youssef EL Yamani <yelyamani@pca.ma>
2022-04-19 10:41:23 +0000

    uniquePathsWithObstacles

--- src/Solution.java
+++ src/Solution.java
@@ -28,6 +28,28 @@ public class Solution {
         return dp[m - 1][n - 1];
     }
 
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid[0][0] == 1) return 0;
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++)
+            dp[i][0] = obstacleGrid[i][0] == 1 ? 0 : 1;
+        for (int j = 0; j < n; j++)
+            dp[0][j] = obstacleGrid[0][j] == 1 ? 0 : 1;
+
+        for (int i = 1; i < m; i++) {
+            A:
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1)
+                    continue A;
+                else
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
 
 }
 
