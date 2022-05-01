38a719621695e44f9223c3b258ab8830fd930955
Youssef EL Yamani <yelyamani@pca.ma>
2022-05-01 21:28:32 +0000

    removeNthFromEnd

--- src/MainSolution.java
+++ src/MainSolution.java
@@ -8,23 +8,80 @@ public class MainSolution {
     public static void main(String[] args) {
     }
 
-    public boolean backspaceCompare(String s, String t) {
-        int n = s.length();
-        StringBuilder str_s = new StringBuilder();
-        StringBuilder str_t = new StringBuilder();
+
+
+    public String reverseWords(String s) {
+        String[] strs = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        int n = strs.length;
         for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            if (c == '#') {
-                if (str_s.length() > 0) str_s.deleteCharAt(str_s.length() - 1);
-            } else str_s.append(c);
+            result.append(reveseWord(strs[i].toCharArray()));
+            if (i < n - 1) result.append(" ");
         }
-        for (int i = 0; i < t.length(); i++) {
-            char c = t.charAt(i);
-            if (c == '#') {
-                if (str_t.length() > 0) str_t.deleteCharAt(str_t.length() - 1);
-            } else str_t.append(c);
+        return result.toString();
+    }
+
+    private char[] reveseWord(char[] toCharArray) {
+        int n = toCharArray.length;
+        int i = 0, j = n - 1;
+        while (i < j) {
+            char t = toCharArray[i];
+            toCharArray[i] = toCharArray[j];
+            toCharArray[j] = t;
+            i++;
+            j--;
         }
-        return str_s.toString().equals(str_t.toString());
+        return toCharArray;
+    }
+
+
+    public int[] twoSum(int[] numbers, int target) {
+        int n = numbers.length;
+        int i = 0, j = n - 1;
+        while (i < j) {
+            int v1 = numbers[i];
+            int v2 = numbers[j];
+            int sum = v1 + v2;
+            if (sum == target) return new int[]{i, j};
+            if (sum > target) j--;
+            else i++;
+        }
+        return new int[]{0, 0};
+    }
+
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] array = new int[n];
+        int i = 0, j = n - 1, index = n - 1;
+        while (i < j) {
+            int leftVal = nums[i] * nums[i];
+            int rightVal = nums[j] * nums[j];
+            if (leftVal > rightVal) {
+                i++;
+                array[index] = leftVal;
+            } else {
+                j--;
+                array[index] = rightVal;
+            }
+            index--;
+        }
+        return array;
+    }
+
+
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int midVal = nums[mid];
+            if (midVal == target) return mid;
+            if (mid > target) {
+                high = mid;
+            } else
+                low = mid + 1;
+        }
+        return low;
     }
 
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,6 @@
 
+import data_struct.ListNode;
+
 import java.io.IOException;
 import java.util.*;
 
@@ -10,50 +12,26 @@ public class Solution {
         System.out.println("abc".contains(s));
     }
 
-    public int minCostConnectPoints(int[][] points) {
-        return KruskalAlgorithm(points);
-    }
-
-    private int KruskalAlgorithm(int[][] points) {
-        Map<Integer, List<int[]>> graph = new HashMap<>();
-        int n = points.length; // node count
-        for (int i = 0; i < n; i++) {
-            int[] point2 = points[i];
-            for (int j = i + 1; j < n; j++) {
-                int[] point1 = points[j];
-                int dist = Math.abs(point2[0] - point1[0]) + Math.abs(point2[1] - point1[1]);
-                int[] edge1 = new int[]{i, j, dist};
-                int[] edge2 = new int[]{j, i, dist};
-                graph.computeIfAbsent(i, k -> new ArrayList<>()).add(edge1);
-                graph.computeIfAbsent(j, k -> new ArrayList<>()).add(edge2);
-            }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode temp = head;
+        ListNode temp2 = head;
+        while (n > 0) {
+            temp2 = temp2.next;
+            n--;
         }
-        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[2] - b[2]); // pg for edges sorted by cost minHeap
-        Set<Integer> visited = new HashSet<>(); // Set to record nodes pushed in mst
-        int cost = 0;
-        if (graph.isEmpty()) return cost;
-        visited.add(0);
-        // add all edges for node 0
-        heap.addAll(graph.get(0));
-        while (!heap.isEmpty()) {
-            // get current min weight
-            int[] curMinEdgeToExplore = heap.poll();
-            int nextNode = curMinEdgeToExplore[1];
-            if (!visited.contains(nextNode)) {
-                visited.add(nextNode);
-                cost += curMinEdgeToExplore[2];
-                // add all edges for next node
-                for (int[] edge : graph.get(nextNode)) {
-                    // we need to check if node already in tree here as well to avoid adding duplicate edges to save time
-                    if (!visited.contains(edge[1])) {
-                        heap.add(edge);
-                    }
-                }
-            }
+        if (temp2 == null) {
+            return head.next;
         }
-        return visited.size() == n ? cost : -1;
-    }
+        while (temp2.next != null) {
+            temp = temp.next;
+            temp2 = temp2.next;
+        }
+        ListNode a = temp.next;
+        ListNode b = a.next;
+        temp.next = b;
+        return head;
 
+    }
 }
 
 
