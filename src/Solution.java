c120e3b1409360e7327f944c048f856086c02a05
Youssef EL Yamani <yelyamani@pca.ma>
2022-05-03 13:05:08 +0000

    findUnsortedSubarray

--- src/Solution.java
+++ src/Solution.java
@@ -8,21 +8,26 @@ public class Solution {
 
 
     public static void main(String[] args) throws IOException {
-
+        int[] array = {2, 6, 4, 8, 10, 9, 15};
+        System.out.println(findUnsortedSubarray(array));
+    }
+    public static int findUnsortedSubarray(int[] nums) {
+        int lo = -1, hi = nums.length - 1;
+        int mn = Integer.MAX_VALUE;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            mn = Math.min(mn, nums[i]);
+            if (nums[i] > mn)
+                lo = i;
+        }
+        int mx = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            mx = Math.max(mx, nums[i]);
+            if (nums[i] < mx)
+                hi = i;
+        }
+        return lo == -1 ? 0 : hi - lo + 1;
     }
 
-    public int[] sortArrayByParity(int[] nums) {
-        int n = nums.length;
-        int[] array = new int[n];
-        int evenIndex = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] % 2 == 0) array[evenIndex++] = nums[i];
-        }
-        for (int i = 0; i < n; i++) {
-            if (nums[i] % 2 == 1) array[evenIndex++] = nums[i];
-        }
-        return array;
-    }
 
 }
 
