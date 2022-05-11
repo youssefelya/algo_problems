af1b3a10bc41bb2dd477e0c1410d991c74c95cf7
Youssef EL Yamani <yelyamani@pca.ma>
2022-05-11 08:26:02 +0100

    combinationSum3

--- src/Solution.java
+++ src/Solution.java
@@ -6,17 +6,18 @@ public class Solution {
 
 
     public static void main(String[] args) {
-
+        System.out.println(combinationSum3(9, 50));
     }
 
-    public List<List<Integer>> combinationSum3(int k, int n) {
+    public static List<List<Integer>> combinationSum3(int k, int n) {
         if (k > n) return new ArrayList<>();
         List<List<Integer>> answer = new ArrayList<>();
-        getCombo(1,k,n,new ArrayList<Integer>());
+        ans = new ArrayList<>();
+        getCombo(1, k, n, new ArrayList<Integer>());
         return ans;
     }
-    private List<List<Integer>> ans = new ArrayList<>();
-    private void getCombo(int i, int k, int target, List<Integer> al) {
+    private static List<List<Integer>> ans;
+    private static void getCombo(int i, int k, int target, List<Integer> al) {
         if (target == 0 && al.size() == k) {
             ans.add(new ArrayList<>(al));
             return;
