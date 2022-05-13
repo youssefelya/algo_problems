895a3e8bd725a54081b43847904c06d039b6d6bc
Youssef EL Yamani <yelyamani@pca.ma>
2022-05-13 15:46:29 +0100

    RedBlackTree

--- src/Solution.java
+++ src/Solution.java
@@ -2,32 +2,32 @@
 
 import java.util.*;
 
+class Parent {
+    {
+        System.out.println("Parent class init block 1");
+    }
+
+    static {
+        System.out.println("Parent  class  static init 2");
+    }
+}
+
+class Child extends Parent {
+    {
+        System.out.println("Child class init block 1");
+    }
+
+    static {
+        System.out.println("Child class  static init 2");
+    }
+}
+
+
 public class Solution {
 
 
     public static void main(String[] args) {
-        System.out.println(combinationSum3(9, 50));
-    }
-
-    public static List<List<Integer>> combinationSum3(int k, int n) {
-        if (k > n) return new ArrayList<>();
-        List<List<Integer>> answer = new ArrayList<>();
-        ans = new ArrayList<>();
-        getCombo(1, k, n, new ArrayList<Integer>());
-        return ans;
-    }
-    private static List<List<Integer>> ans;
-    private static void getCombo(int i, int k, int target, List<Integer> al) {
-        if (target == 0 && al.size() == k) {
-            ans.add(new ArrayList<>(al));
-            return;
-        }
-        if (i <= 9 && target > 0) {
-            getCombo(i + 1, k, target, al);
-            al.add(i);
-            getCombo(i + 1, k, target - i, al);
-            al.remove(al.size() - 1);
-        }
+        new Child();
     }
 
 
