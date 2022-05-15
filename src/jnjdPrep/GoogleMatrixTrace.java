2de669f69b17266f225e05467ad363d4ef6a11ac
Youssef <youssefelyamani96@gmail.com>
2022-05-15 11:57:42 +0100

    deepestLeavesSum

--- src/jnjdPrep/GoogleMatrixTrace.java
+++ src/jnjdPrep/GoogleMatrixTrace.java
@@ -3,7 +3,7 @@ package jnjdPrep;
 import java.io.*;
 import java.util.Arrays;
 
-public class Solution {
+public class GoogleMatrixTrace {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
