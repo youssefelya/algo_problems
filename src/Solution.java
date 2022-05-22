687fc1a5087f4b82f607a6a0fdc717bf5f1b01af
Youssef <youssefelyamani96@gmail.com>
2022-05-22 15:41:52 +0100

    getNewPassword

--- src/Solution.java
+++ src/Solution.java
@@ -1,34 +1,46 @@
 import data_struct.TreeNode;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
 
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(bf.readLine());
+        for (int test = 1; test <= testCase; test++) {
+            int N = Integer.parseInt(bf.readLine());
+            String oldPassword = bf.readLine();
+            System.out.println("Case #" + test + ": " + getNewPassword(oldPassword));
+        }
     }
 
-    public int countSubstrings(String s) {
-        int n = s.length();
-        int count = 0;
+    private static String getNewPassword(String oldPassword) {
+        boolean hasSpecialCharacter = false;
+        boolean hasDigit = false;
+        boolean hasUpperCase = false;
+        boolean hasLowerCase = false;
+        StringBuffer stringBuffer = new StringBuffer(oldPassword);
+        int n = oldPassword.length();
         for (int i = 0; i < n; i++) {
-            for (int j = 1; j < n; j++) {
-                if (isPalandrom(s, i, j)) count++;
-            }
+            char c = oldPassword.charAt(i);
+            if (Character.isDigit(c)) hasDigit = true;
+            else if (Character.isAlphabetic(c)) {
+                char upper = Character.toUpperCase(c);
+                if (c == upper) hasUpperCase = true;
+                if (c != upper) hasLowerCase = true;
+            } else if (c == '#' || c == '@' || c == '*' || c == '&') hasSpecialCharacter = true;
         }
-        return count;
-    }
-
-    private boolean isPalandrom(String s, int i, int j) {
-        if (j - i == 1) return true;
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) return false;
-            i++;
-            j--;
-        }
-        return true;
+        if (!hasDigit) stringBuffer.append("1");
+        if (!hasLowerCase) stringBuffer.append("a");
+        if (!hasUpperCase) stringBuffer.append("A");
+        if (!hasSpecialCharacter) stringBuffer.append("@");
+        while (stringBuffer.length() < 7) stringBuffer.append("1");
+        return stringBuffer.toString();
     }
 
 
