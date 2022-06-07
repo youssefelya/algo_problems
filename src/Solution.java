dd64f7aa1b5b5528afadffb502ff8958dd88ddc5
Youssef EL Yamani <yelyamani@pca.ma>
2022-06-07 19:01:45 +0100

    merge

--- src/Solution.java
+++ src/Solution.java
@@ -6,27 +6,26 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-
     }
 
-
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        bfs(root, map, 0, true);
-        List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < map.size(); i++) ans.add(i, map.get(i));
-        return ans;
-    }
-
-    public void bfs(TreeNode root, Map<Integer, List<Integer>> map, int l, boolean r) {
-        if (root == null) return;
-        map.computeIfAbsent(l, x -> new ArrayList<>()).add(root.val);
-        if (r) {
-            bfs(root.right, map, l + 1, false);
-            bfs(root.left, map, l + 1, true);
-        } else {
-            bfs(root.left, map, l + 1, true);
-            bfs(root.right, map, l + 1, false);
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1;
+        int index = nums1.length - 1;
+        while (index >= 0) {
+            if (j < 0) {
+                nums1[index] = nums1[i];
+                i--;
+            } else if (i < 0) {
+                nums1[index] = nums2[j];
+                j--;
+            } else if (nums1[i] > nums2[j]) {
+                nums1[index] = nums1[i];
+                i--;
+            } else {
+                nums1[index] = nums2[j];
+                j--;
+            }
+            index--;
         }
     }
 
