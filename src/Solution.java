28b00fb84a1f95cea294082f20df405df8b58451
Youssef EL Yamani <yelyamani@pca.ma>
2022-06-09 08:45:21 +0100

    twoSum2

--- src/Solution.java
+++ src/Solution.java
@@ -9,49 +9,17 @@ public class Solution {
     }
 
 
-    public Node copyRandomList(Node head) {
-        if (head == null) return null;
-        map = new HashMap<>();
-        Node temp = copyNode(head);
-        Node t = temp;
-        while (head != null) {
-            if (map.containsKey(t.val))
-                t.random = map.get(t.val);
-            t = t.next;
-            head = head.next;
+    public int[] twoSum(int[] numbers, int target) {
+        int n = numbers.length;
+        int i = 0, j = n - 1;
+        while (i < j) {
+            int v = numbers[i] + numbers[j];
+            if (v == target) return new int[]{i, j};
+            if (v > target) j--;
+            else i++;
         }
-        return temp;
+        return new int[]{};
     }
-
-    Map<Integer, Node> map;
-
-    private Node copyNode(Node head) {
-        if (head == null) ;
-        Node t = new Node(head.val);
-        t.next = copyNode(head.next);
-        map.put(head.val, t);
-        return t;
-    }
-
-
-    Set<Integer> set = new HashSet<>();
-
-    public boolean findTarget(TreeNode root, int k) {
-        if (root == null) return false;
-        if (set.contains(root.val)) return true;
-        set.add(k - root.val);
-        return findTarget(root.left, k) || findTarget(root.right, k);
-    }
-
-    static class Node {
-        Node next;
-        Node random;
-        int val;
-
-        public Node(int val) {
-        }
-    }
-
 
 }
 
